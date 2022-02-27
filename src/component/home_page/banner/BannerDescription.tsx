993255cd5e277c90aa3d07bfa825652902834e2a
Nabin Kawan <nabin.kawan.9@gmail.com>
2022-02-27 08:16:56 +0545

    Fix banner responsive issue

--- src/component/home_page/banner/BannerDescription.tsx
+++ src/component/home_page/banner/BannerDescription.tsx
@@ -6,7 +6,7 @@ export default function BannerDescription() {
   return (
     <div className="flex flex-col space-y-3  items-center text-white font-sans">
       <p className="lg:text-2xl sm:text-xl font-medium ">Sign Gesture Detection</p>
-      <p className="lg:text-lg sm:text-base font-normal">Detects the gesture sign movement and classifies it</p>
+      <p className="lg:text-lg sm:text-base text-center font-normal">Detects the gesture sign movement and classifies it</p>
     </div>
   );
 }
