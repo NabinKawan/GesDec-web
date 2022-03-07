cc3399a7e8ed726ecbdd52146d79bb2802276603
Nabin Kawan <nabin.kawan.9@gmail.com>
2022-03-07 12:23:35 +0545

    Change banner details

--- src/component/home_page/banner/BannerDescription.tsx
+++ src/component/home_page/banner/BannerDescription.tsx
@@ -3,6 +3,8 @@ import { motion } from 'framer-motion';
 // Homepage banner description
 
 export default function BannerDescription() {
+  const scrollToBottom = () =>
+    window.scrollTo({ top: 1.6 * window.screen.height, behavior: 'smooth' });
   return (
     <div className="flex flex-col space-y-4  items-start justify-start text-white font-sans lg:pt-12 sm:pt-4">
       <p className="lg:text-3xl sm:text-xl font-medium ">Gesture Detection and Recognition</p>
@@ -15,8 +17,11 @@ export default function BannerDescription() {
       ></motion.div>
       <p className="lg:text-base sm:text-sm text-start text-gray-400  font-extralight ">
         Our model is mainly focused on sign gestures. You can predict 10 of the sign gestures. The
-        demo video is shown for all of the images in the right side of the banner. You can also
-        check our model details by clicking <span className="text-primary">Go to details</span>{' '}
+        demo video is shown for all of the gestures in the right side of the banner. You can also
+        check our model details by clicking{' '}
+        <span className="text-primary font-light cursor-pointer" onClick={scrollToBottom}>
+          Go to details
+        </span>{' '}
         button.
       </p>
     </div>
